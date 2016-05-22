8f2078c763fed6424303aa4a9a762f90ac623d89
sijunhe <sijun.he@hotmail.com>
2016-05-21 23:41:11 -0700

    combined features

--- v5/computeFeatures.py
+++ v5/computeFeatures.py
@@ -3,65 +3,64 @@ from computeLegalMove import *
 import numpy as np
 
 def computeFeatures(board):
-	numFeature = 9
-	features = np.ones((numFeature))
+	numFeature = 5
+	features = np.zeros((numFeature))
 	### Compute every feature
-
+	features[0] = 1
 	'''distance to the end - position_1'''
 	for (i,j) in board.PositionOne:
 		features[1] -= (board.height - 1 - i)**2
 	'''distance to the end - position_2'''
 	for (i,j) in board.PositionTwo:
-		features[2] += i**2
+		features[1] += i**2
 
 	'''distance to the center - position_1'''
 	for (i,j) in board.PositionOne:
-		features[3] -= (j-board.midElement)**2
+		features[2] -= (j-board.midElement)**2
 	'''distance to the center - position_2'''
 	for (i,j) in board.PositionTwo:
-		features[4] += (j-board.midElement)**2
+		features[2] += (j-board.midElement)**2
 
 	'''variance of pieces distribution - position_1'''
 	for (i,j) in board.PositionOne:
 		for (k,l) in board.PositionOne:
-				features[5] -= (i-k)**2 + (j-l)**2
+				features[3] -= (i-k)**2 + (j-l)**2
 	'''variance of pieces distribution - position_2'''
 	for (i,j) in board.PositionTwo:
 		for (k,l) in board.PositionTwo:
-				features[6] += (i-k)**2 + (j-l)**2
+				features[3] += (i-k)**2 + (j-l)**2
 
 	return features
 
 
 
 def computeFeaturesFull(board):
-	numFeature = 9
-	features = np.ones((numFeature))
+	numFeature = 5
+	features = np.zeros((numFeature))
 	### Compute every feature
-
+	features[0] = 1
 	'''distance to the end - position_1'''
 	for (i,j) in board.PositionOne:
 		features[1] -= (board.height - 1 - i)**2
 	'''distance to the end - position_2'''
 	for (i,j) in board.PositionTwo:
-		features[2] += i**2
+		features[1] += i**2
 
 	'''distance to the center - position_1'''
 	for (i,j) in board.PositionOne:
-		features[3] -= (j-board.midElement)**2
+		features[2] -= (j-board.midElement)**2
 	'''distance to the center - position_2'''
 	for (i,j) in board.PositionTwo:
-		features[4] += (j-board.midElement)**2
+		features[2] += (j-board.midElement)**2
 
 	'''variance of pieces distribution - position_1'''
 	for (i,j) in board.PositionOne:
 		for (k,l) in board.PositionOne:
-				features[5] -= (i-k)**2 + (j-l)**2
+				features[3] -= (i-k)**2 + (j-l)**2
 	'''variance of pieces distribution - position_2'''
 	for (i,j) in board.PositionTwo:
 		for (k,l) in board.PositionTwo:
-				features[6] += (i-k)**2 + (j-l)**2
-	
+				features[3] += (i-k)**2 + (j-l)**2
 
 	'''total furthest moves - position_1'''
 	best_move = 0
@@ -70,7 +69,7 @@ def computeFeaturesFull(board):
 		#print (i1,j1), (i2,j2)
 		if i2 - i1 > best_move:
 			best_move = i2 - i1
-	features[7] = best_move
+	features[4] += best_move
 
 	'''total furthest moves - position_2'''
 	best_move = 0
@@ -79,7 +78,7 @@ def computeFeaturesFull(board):
 		#print (i1,j1), (i2,j2)
 		if i1 - i2 > best_move:
 			best_move = i1 - i2
-	features[8] = best_move
+	features[4] -= best_move
 
 
 	return features
