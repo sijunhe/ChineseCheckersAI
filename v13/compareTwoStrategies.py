246fd2df0dc0fcaecaa2d1715f16cc477858b65d
hyin-stanford <hyin.stanford@gmail.com>
2016-05-29 01:08:55 -0700

    compare two strategies modified

--- v13/compareTwoStrategies.py
+++ v13/compareTwoStrategies.py
@@ -7,18 +7,24 @@ import numpy as np
 from strategies import *
 
 
-totalGames = 10
+totalGames = 1
 gameCount = [0, 0, 0]	# The 0-th element denotes the number of games played so far, i-th denotes the number of games player i wins, i = 1, 2.
 cantGo1 = []			# The set of moves player 1 can't make
 cantGo2 = []			# The set of moves player 2 can't make
 playerOneExtraTurn = []	# Number of moves player 1 loses in each game, negative means player 2 loses
 
 '''
+Player One's weights: 
+'''
+aaaaa = [10, 1, 1]
+weights1 = np.array(aaaaa)
+weights1 = weights1 / np.linalg.norm(weights1)
+'''
 Player Two's weights: 
 '''
 aaaaa = [10, 1, 1]
-weights = np.array(aaaaa)
-weights = weights / np.linalg.norm(weights)
+weights2 = np.array(aaaaa)
+weights2 = weights2 / np.linalg.norm(weights2)
 
 
 
@@ -34,14 +40,18 @@ while gameCount[0] < totalGames:
 		timeTurnStart = time.time()
 		print('\nGame No. ' + str(gameCount[0]) + ', turn No. ' + str(turn))
 		if (player == 1) :
-			move = findMove_GreedyRandom(boardNow, player, 2)		## This can be too strong ...
 			# move = findMove_GreedyRandom(boardNow, player, 1)
+			# move = findMove_GreedyRandom(boardNow, player, 2)
+			# move = findMove_MiniMax(boardNow, player, weights1, 2, cantGo1)	## Minimax strategy throughout
+			move = findMove_Advanced(boardNow, player, weights1, 2, cantGo2)		## Most advanced strategy
 			cantGo1.append(move)
 			if (len(cantGo1) >= 5) :
 				cantGo1.pop(0)
 		else :
-			# move = findMove_MiniMax(boardNow, player, weights, 2, cantGo2)	## Minimax strategy throughout
-			move = findMove_Advanced(boardNow, player, weights, 2, cantGo2)		## Most advanced strategy
+			# move = findMove_GreedyRandom(boardNow, player, 1)
+			# move = findMove_GreedyRandom(boardNow, player, 2)
+			move = findMove_MiniMax(boardNow, player, weights2, 2, cantGo1)	## Minimax strategy throughout
+			# move = findMove_Advanced(boardNow, player, weights2, 2, cantGo2)		## Most advanced strategy
 			cantGo2.append(move)
 			if (len(cantGo2) >= 5) :
 				cantGo2.pop(0)
